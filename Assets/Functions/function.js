80c476167a7bf4548b06c4299176423d4c0b1f9f
joonwhen <67354979+joonwhen@users.noreply.github.com>
2020-09-20 01:13:27 +0800

    Added New JS Practice Examples

--- Assets/Functions/function.js
+++ Assets/Functions/function.js
@@ -38,8 +38,77 @@ function Timer_Form(){
   + minutes + "m " + seconds + "s ";
 }
 
-function Vector_Calc(arr1, arr2, testcase){
-  var arr3 = arr1 * arr2;
-  var x = arr3.reduce();
-  alert(x);
+function reverse_string(){
+  x = document.getElementById("string_to_be_reversed").value;
+  var x_str = x.toString();
+  //var y = [];
+  //for (i = 0; i < x_str.length; i++){
+  //  y[x.length - i - 1] = " " + x_str.charAt(i) ;
+  //}
+  y = x_str.split("").reverse().join("");
+  document.getElementById("reverse_display").innerHTML = y;
+}
+
+function pig_latin(){
+  x = document.getElementById("pig_latin_string").value;
+  var x_str = x.toString();
+  var y_str = "-" + x_str.charAt(0) + "ay";
+  var result = x_str.substring(1) + y_str;
+  document.getElementById("pig_latin_display").innerHTML = result;
+}
+
+function vowel_counter(){
+  var x = document.getElementById("vowel_counter_input").value;
+  var a_count = e_count = i_count = o_count = u_count = vowels = 0;
+  var x_str = x.split("");
+  for(i = 0; i < x_str.length; i++){
+    if (x_str[i] == "a" || x_str[i] == "A"){
+      a_count+=1;
+      vowels+=1;
+    }
+    else if (x_str[i] == "e" || x_str[i] == "E"){
+      e_count+=1;
+      vowels+=1;
+    }
+    else if (x_str[i] == "i" || x_str[i] == "I"){
+      i_count+=1;
+      vowels+=1;
+    }
+    else if (x_str[i] == "o" || x_str[i] == "O"){
+      o_count+=1;
+      vowels+=1;
+    }
+    else if (x_str[i] == "u" || x_str[i] == "U"){
+      u_count+=1;
+      vowels+=1;
+    }
+  }
+  document.getElementById("vowel_counter_display").innerHTML =
+  "The number of 'a' is: " + a_count + ".<br>" +
+  "The number of 'e' is: " + e_count + ".<br>" +
+  "The number of 'i' is: " + i_count + ".<br>" +
+  "The number of 'o' is: " + o_count + ".<br>" +
+  "The number of 'u' is: " + u_count + ".<br>" +
+  "The total number of vowels is: " + vowels + ".<br>";
+}
+
+function palindrome_checker(){
+  x = document.getElementById("palindrome_checker_input").value;
+  var x_str = x.toString().replace(/[\W_ ]/g, "").toLowerCase();
+  var y_str = x_str.split("").reverse().join("");
+  var result = 0;
+  for (i = 0; i < x_str.length; i++){
+    if (x_str[i] == y_str[i]){
+      result = 1;
+    } else {
+      result = 0;
+      break;
+    }
+  }
+  if (result == 0){
+    document.getElementById("palindrome_checker_display").innerHTML = "<span style = 'color:#87a5c1'>"+x+"</span>" + " is not a palindrome.";
+  }
+  else{
+    document.getElementById("palindrome_checker_display").innerHTML = "<span style = 'color:#87a5c1'>"+x+"</span>" + " is a palindrome.";
+  }
 }
